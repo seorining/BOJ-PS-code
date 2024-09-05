8920f94a75fd8301fdadeafc2e6d618ea6b060bb
seorining <train1312@naver.com>
2024-09-05 22:57:37 +0900

    star

--- 10993.py
+++ 10993.py
@@ -1,13 +1,38 @@
-def star(i):
-    if i == 1:
-        print("*")
-    elif i == 2:
-        for i in range(3):
-            for j in range(i):
-                print(" ", end="")
-            for j in range(3*2 -2*i-1):
-                print("*", end="")
-            print("*")
+def paintStar(height, width, n):
+    if n == 1:
+        starBoard[height][width] = '*'
+        return
+    
+    widthSize = 2 ** (n+1) - 3
+    heightSize = 2 ** n - 1
+    resize = 2**(n-1)-1
+    
+    if ~n&1:
+        for i in range(widthSize):
+            starBoard[height][width+i] = '*'
+        for i in range(1, heightSize):
+            starBoard[height+i][width + widthSize - 1 - i] = '*'
+        for i in range(1, heightSize-1):
+            starBoard[height+i][width+i] = '*'
+        paintStar(height+resize , width+resize+1, n-1)
+    else:
+        for i in range(widthSize):
+            starBoard[height][width+i] = '*'
+        for i in range(1, heightSize):
+            starBoard[height-i][width+widthSize-1-i] = '*'
+        for i in range(1, heightSize -1):
+            starBoard[height-i][width+i] = '*'
+        paintStar(height-resize , width+resize+1, n-1)
 
-print(star(2))
+##main##
+n = int(input())
+width = 2 ** (n+1) - 3
+height = 2 ** n - 1
 
+starBoard = [[' ' for _ in range(width)]for _ in range(height)]
+            
+if ~n&1:paintStar(0,0,n)
+else : paintStar(height-1,0,n)
+
+for _ in starBoard:
+    print(''.join(_).rstrip(' '))
