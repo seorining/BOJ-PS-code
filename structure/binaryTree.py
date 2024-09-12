4170eb93ac5218b0b86978dbb88f0cb415bff169
seorining <train1312@naver.com>
2024-09-13 07:59:50 +0900

    change

--- structure/binaryTree.py
+++ structure/binaryTree.py
@@ -1,9 +1,1 @@
-class Node:
-    def __init__(self, item=0):
-        self.item = item
-        self.left = None
-        self.right = None
-        
-class BinaryTree():
-    def __init__(self):
-        self.root = None
+dd
