e51da424782d9c55859132ffbafdbbd82e6d8f83
seorining <train1312@naver.com>
2024-09-20 02:20:27 +0900

    solve

--- 11727.py
+++ 11727.py
@@ -1,12 +1,13 @@
 import sys
 input = sys.stdin.readline
+
 n = int(input())
 
 dp = [0] * (n+2)
 dp[1] = 1
-dp[2] = 2
+dp[2] = 3
 
 for i in range(3, n+1):
-    dp[i] = dp[i-1]%10007 + dp[i-2]%10007
-        
+    dp[i] = (dp[i-1]%10007) + ((2*dp[i-2])%10007)
+    
 print(dp[n]%10007)
